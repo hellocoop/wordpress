c715b3dd6ecbc4ed003986cd27ad8fe089c8f9ee
Marius Scurtescu <marius.scurtescu@gmail.com>
2022-11-10 17:54:26 -0800

    add Hello Login specific deploy and compose

--- test-drive/deploy.sh
+++ test-drive/deploy.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-DOCKER_VOLUME=/var/lib/docker/volumes/wordpress_wordpress/_data/wp-content/plugins/hello-login/
+DOCKER_VOLUME=./plugin
 
-cd wordpress || exit
-
-sudo mkdir -p $DOCKER_VOLUME
-#sudo cp -v -u CHANGELOG.md docker-compose.wp-env.yml docker-compose.yml hello-login.php readme.txt $DOCKER_VOLUME
-sudo cp -v -u CHANGELOG.md hello-login.php readme.txt $DOCKER_VOLUME
-sudo cp -r -v -u css includes languages $DOCKER_VOLUME
-sudo chown -R www-data:www-data $DOCKER_VOLUME
-
+cp -v -u ../CHANGELOG.md ../hello-login.php ../readme.txt $DOCKER_VOLUME
+cp -r -v -u ../css ../includes ../languages $DOCKER_VOLUME
