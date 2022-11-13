04431d12a27dd5ceaac3efcab69f9d158af6a6fb
Professor Farnsworth <professor@futurama.dev>
2022-11-12 23:30:56 -0800

    allow WOrdPress to delete the plugin

--- test-drive/install.sh
+++ test-drive/install.sh
@@ -3,3 +3,7 @@
 # copy the hello-login plugin files into plugin folder
 cp -v ../CHANGELOG.md ../hello-login.php ../readme.txt plugin/
 cp -r -v ../css ../includes ../languages plugin/
+
+# allow WordPress to delete the plugin
+chmod a+w -R plugin/
+
