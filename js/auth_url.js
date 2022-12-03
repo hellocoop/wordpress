d9eb04d2d409bb5bc015a7a84cf9104458647b5c
Marius Scurtescu <marius.scurtescu@gmail.com>
2022-12-02 17:37:38 -0800

    pass current path and query to auth_url REST API

--- js/auth_url.js
+++ js/auth_url.js
@@ -1,6 +1,6 @@
 async function navigateToHelloAuthRequestUrl() {
 	try {
-		const res = await fetch('/wp-json/hello-login/v1/auth_url?redirect_to_path=%2F')
+		const res = await fetch('/wp-json/hello-login/v1/auth_url?redirect_to_path=' + encodeURIComponent(location.pathname+location.search))
 		const json = await res.json()
 		window.location.href = json['url']
 	} catch (err) {
