24f898ad38e63c38f6b6fef513c73eeee5d7886a
Marius Scurtescu <marius.scurtescu@gmail.com>
2022-12-07 14:25:33 -0800

    fix position of logout message

--- includes/hello-login-login-form.php
+++ includes/hello-login-login-form.php
@@ -65,7 +65,7 @@ class Hello_Login_Login_Form {
 		$login_form = new self( $logger, $settings, $client_wrapper );
 
 		// Alter the login form as dictated by settings.
-		add_filter( 'login_message', array( $login_form, 'handle_login_page' ), 99 );
+		add_filter( 'login_messages', array( $login_form, 'handle_login_page' ), 99 );
 
 		// Add a shortcode for the login button.
 		add_shortcode( 'hello_login_button', array( $login_form, 'make_login_button' ) );
