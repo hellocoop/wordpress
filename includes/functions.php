d9dba8d669c71034ac7acbcd596fb2b27c32dceb
Marius Scurtescu <marius.scurtescu@gmail.com>
2022-12-12 14:12:59 -0800

    add link/unlink buttons to profile page

--- includes/functions.php
+++ includes/functions.php
@@ -17,3 +17,25 @@ function hello_login_enqueue_scripts_and_styles() {
 add_action( 'wp_enqueue_scripts', 'hello_login_enqueue_scripts_and_styles' );
 add_action( 'login_enqueue_scripts', 'hello_login_enqueue_scripts_and_styles' );
 add_action( 'admin_enqueue_scripts', 'hello_login_enqueue_scripts_and_styles' );
+
+function hello_login_user_profile( $profileuser ) {
+	$api_url = rest_url( 'hello-login/v1/auth_url' );
+	$hello_user_id = get_user_meta( $profileuser->ID, 'hello-login-subject-identity', true );
+	?>
+	<h2>Hellō Login</h2>
+	<table class="form-table">
+		<tr>
+			<th>Hellō Wallet</th>
+			<td>
+				<?php if ( empty( $hello_user_id ) ) { ?>
+					<button class="hello-btn" data-label="ō&nbsp;&nbsp;&nbsp;Link Hellō" onclick="navigateToHelloAuthRequestUrl('<?php print esc_js( $api_url ); ?>', '')"></button>
+				<?php } else { ?>
+					<button class="hello-btn" data-label="ō&nbsp;&nbsp;&nbsp;Unlink Hellō" onclick="navigateToHelloAuthRequestUrl('<?php print esc_js( $api_url ); ?>', '')"></button>
+				<?php } ?>
+			</td>
+		</tr>
+	</table>
+	<?php
+}
+
+add_action( 'show_user_profile', 'hello_login_user_profile' );
