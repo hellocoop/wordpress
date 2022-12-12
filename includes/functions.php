b2720f689736565ddfcc82776226b5a15a94f776
Marius Scurtescu <marius.scurtescu@hello.coop>
2022-12-12 15:54:47 -0800

    add Hellō link/unlink buttons to profile page (#78)

--- includes/functions.php
+++ includes/functions.php
@@ -17,3 +17,32 @@ function hello_login_enqueue_scripts_and_styles() {
 add_action( 'wp_enqueue_scripts', 'hello_login_enqueue_scripts_and_styles' );
 add_action( 'login_enqueue_scripts', 'hello_login_enqueue_scripts_and_styles' );
 add_action( 'admin_enqueue_scripts', 'hello_login_enqueue_scripts_and_styles' );
+
+/**
+ * Show Hellō account linking controls on the profile page.
+ *
+ * @param WP_User $profileuser The user whose profile is being edited.
+ * @return void
+ */
+function hello_login_user_profile( $profileuser ) {
+	$api_url = rest_url( 'hello-login/v1/auth_url' );
+	$hello_user_id = get_user_meta( $profileuser->ID, 'hello-login-subject-identity', true );
+	$unlink_url = site_url( '?hello-login=unlink' );
+	?>
+	<h2>Hellō Login</h2>
+	<table class="form-table">
+		<tr>
+			<th>Hellō Wallet</th>
+			<td>
+				<?php if ( empty( $hello_user_id ) ) { ?>
+					<button type="button" class="hello-btn" data-label="ō&nbsp;&nbsp;&nbsp;Link Hellō" onclick="navigateToHelloAuthRequestUrl('<?php print esc_js( $api_url ); ?>', '')"></button>
+				<?php } else { ?>
+					<button type="button" class="button" onclick="parent.location='<?php print esc_js( $unlink_url ); ?>'">ō&nbsp;&nbsp;&nbsp;Unlink Hellō</button>
+				<?php } ?>
+			</td>
+		</tr>
+	</table>
+	<?php
+}
+
+add_action( 'show_user_profile', 'hello_login_user_profile' );
