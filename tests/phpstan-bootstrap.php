b0faf24eedcffb066027b0723270bef2d99f574f
Viktor Szépe <viktor@szepe.net>
2023-04-27 01:07:36 +0200

    Go unconditional in bootstrap
    Signed-off-by: Viktor Szépe <viktor@szepe.net>

--- tests/phpstan-bootstrap.php
+++ tests/phpstan-bootstrap.php
@@ -11,16 +11,16 @@
  */
 
 // Define WordPress language directory.
-defined( 'WP_LANG_DIR' ) || define( 'WP_LANG_DIR', 'wordpress/src/wp-includes/languages/' );
+define( 'WP_LANG_DIR', 'wordpress/src/wp-includes/languages/' );
 
-defined( 'COOKIE_DOMAIN' ) || define( 'COOKIE_DOMAIN', 'localhost' );
-defined( 'COOKIEPATH' ) || define( 'COOKIEPATH', '/');
+define( 'COOKIE_DOMAIN', 'localhost' );
+define( 'COOKIEPATH', '/');
 
 // Define Plugin Globals.
-defined( 'HELLO_LOGIN_CLIENT_ID' ) || define( 'HELLO_LOGIN_CLIENT_ID', bin2hex( random_bytes( 32 ) ) );
-defined( 'HELLO_LOGIN_ENDPOINT_LOGIN_URL' ) || define( 'HELLO_LOGIN_ENDPOINT_LOGIN_URL', 'https://wallet.hello.coop/authorize' );
-defined( 'HELLO_LOGIN_ENDPOINT_TOKEN_URL' ) || define( 'HELLO_LOGIN_ENDPOINT_TOKEN_URL', 'https://wallet.hello.coop/oauth/token' );
-defined( 'HELLO_LOGIN_CLIENT_SCOPE' ) || define( 'HELLO_LOGIN_CLIENT_SCOPE', '' );
-defined( 'HELLO_LOGIN_LINK_EXISTING_USERS' ) || define( 'HELLO_LOGIN_LINK_EXISTING_USERS', 1 );
-defined( 'HELLO_LOGIN_CREATE_IF_DOES_NOT_EXIST' ) || define( 'HELLO_LOGIN_CREATE_IF_DOES_NOT_EXIST', 1 );
-defined( 'HELLO_LOGIN_REDIRECT_USER_BACK' ) || define( 'HELLO_LOGIN_REDIRECT_USER_BACK', 1 );
+define( 'HELLO_LOGIN_CLIENT_ID', bin2hex( random_bytes( 32 ) ) );
+define( 'HELLO_LOGIN_ENDPOINT_LOGIN_URL', 'https://wallet.hello.coop/authorize' );
+define( 'HELLO_LOGIN_ENDPOINT_TOKEN_URL', 'https://wallet.hello.coop/oauth/token' );
+define( 'HELLO_LOGIN_CLIENT_SCOPE', '' );
+define( 'HELLO_LOGIN_LINK_EXISTING_USERS', 1 );
+define( 'HELLO_LOGIN_CREATE_IF_DOES_NOT_EXIST', 1 );
+define( 'HELLO_LOGIN_REDIRECT_USER_BACK', 1 );
