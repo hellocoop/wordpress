688395e4b645668a932ebda1c16ae08a55572c15
Marius Scurtescu <marius.scurtescu@gmail.com>
2023-04-26 14:30:37 -0700

    ignore jti as saved claim

--- includes/hello-login-client-wrapper.php
+++ includes/hello-login-client-wrapper.php
@@ -912,7 +912,7 @@ class Hello_Login_Client_Wrapper {
 	 */
 	public function save_extra_claims( int $uid, array $user_claim ) {
 		foreach ( $user_claim as $key => $value ) {
-			if ( ! in_array( $key, array( 'iss', 'sub', 'aud', 'exp', 'iat', 'auth_time', 'nonce', 'acr', 'amr', 'azp' ) ) ) {
+			if ( ! in_array( $key, array( 'iss', 'sub', 'aud', 'exp', 'iat', 'jti', 'auth_time', 'nonce', 'acr', 'amr', 'azp' ) ) ) {
 				if ( update_user_meta( $uid, 'hello-login-claim-' . $key, $value ) ) {
 					$this->logger->log( 'User claim saved as meta: hello-login-claim-' . $key . ' = ' . $value, 'user-claims' );
 				}
