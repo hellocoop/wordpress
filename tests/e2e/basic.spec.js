8bea51b19b2a6b3f8c09fa23036e95ad161aae18
Marius Scurtescu <marius.scurtescu@gmail.com>
2023-05-10 16:34:32 -0700

    add basic end-to-end playwright test

---
+++ tests/e2e/basic.spec.js
@@ -0,0 +1,33 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('login page not modified', async ({ page }) => {
+	await page.goto('http://localhost:8888/wp-login.php');
+
+	const body = page.locator('body');
+
+	await expect(body).toContainText("Log In");
+	await expect(body).not.toContainText("Hellō");
+});
+
+test('plugin installed and active', async ({ page }) => {
+	await page.goto('http://localhost:8888/wp-login.php');
+
+	// Sign in as admin
+	await page.getByLabel('Username or Email Address').click();
+	await page.getByLabel('Username or Email Address').fill('admin');
+	await page.getByLabel('Password', { exact: true }).click();
+	await page.getByLabel('Password', { exact: true }).fill('password');
+	await page.getByRole('button', { name: 'Log In' }).click();
+
+	// Navigate to plugins page
+	await page.getByRole('link', { name: 'Plugins', exact: true }).click();
+
+	// check if Hellō Login is present and active
+	const pluginRow = page.getByRole('row', { name: 'Hellō Login' });
+	await expect(pluginRow).toHaveCount(1);
+	await expect(pluginRow).toContainText(/Deactivate/);
+
+	// TODO: fix Settings link
+	// await expect(pluginRow).toContainText(/Settings/);
+});
