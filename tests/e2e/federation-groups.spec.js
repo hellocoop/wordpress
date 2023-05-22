c41ef4e22125d861fe69d259c3df7c86752f906e
Marius Scurtescu <marius.scurtescu@gmail.com>
2023-05-22 16:45:06 -0700

    add one more federation org with groups

--- tests/e2e/federation-groups.spec.js
+++ tests/e2e/federation-groups.spec.js
@@ -23,7 +23,7 @@ test('should create groups with groups sync', async ({ page, request }) => {
 	await page.getByRole('button', { name: 'Log In' }).click();
 
 
-	// Navigate to Hellō Login settings page and check that it is not configured.
+	// Navigate to Hellō Login federation settings page and check that it is configured.
 	await page.getByRole('navigation').getByRole('link', { name: 'Settings', exact: true }).click();
 	await page.getByRole('link', { name: 'Hellō Login', exact: true }).click();
 
@@ -34,4 +34,24 @@ test('should create groups with groups sync', async ({ page, request }) => {
 
 	await expect(page.getByText('Admins', { exact: true })).toHaveCount(1);
 	await expect(page.getByText('Marketing', { exact: true })).toHaveCount(1);
+
+	const result2 = await request.post('/?hello-login=event', {
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/secevent+jwt'
+		},
+		data: "eyJhbGciOiJSUzI1NiIsImN0eSI6IkpXVCIsInR5cCI6ImFwcGxpY2F0aW9uL3NlY2V2ZW50K2p3dCJ9.eyJpc3MiOiJodHRwczovL2lzc3Vlci5oZWxsby5jb29wIiwiYXVkIjoiNGMxZWM5M2ItMjcxNC00ZDcyLTk1ODItYjA5YzUxNWM1YmQ4IiwianRpIjoiRWtCbXdUNzRBYzI1elJHNnBmMlBQIiwiaWF0IjoxNjc5MTAzNjkyLCJldmVudHMiOnsiaHR0cHM6Ly9oZWxsby5jb29wL2ZlZGVyYXRpb24vZ3JvdXBzL3N5bmMiOnsiZ3JvdXBzIjpbeyJ2YWx1ZSI6IjEyMzQiLCJkaXNwbGF5IjoiRW5naW5lZXJpbmcifSx7InZhbHVlIjoiMzQ1NiIsImRpc3BsYXkiOiJTYWxlcyJ9XSwib3JnIjoiZXhhbXBsZS5vcmcifX19.FYallmfu7gu4K7TuY_6As9u5uxz8N7OsEB6z5nW8GDlgOoyKpEUIACWmlovyWqH1XClzyaOuAwKTBXug0HdjEKZv-ggT_bI5mBIlnQc_-o7J-0eXHVLgTH0vKcnXuTnkbFNr3OqTZqiWEDAZp5S6fIZUqo9sjD1Jmnr91x-wOKJheGbzzE3gNp2BIbrO8U_pUt2EQJYTP6Ywnrxo6NjKVZB9Dr-G40pXWrUO5ZWmRbeqNboLi_nXu7WOSHBfMPwLMgBHmAFUQDtFqem_XgypDdm9dBrwq2C3FyjCt_nVy_yQYphQN9I9PV2nnwp8f73zb3Y2CdM1bupWbJH5CeNU275de4hhyb4Kaa0GyMgAu3AJ6FN3PCvxvRJy8s1m88NgbGLAnbn9ybjzPS7Qw2Pv-zfjO76uF0cshS_JeXeKqnq1imPDjCxG9wm0Q_2h36KxQ-QrZL0Kth5zF-yOc115gvXRJZCiKGbO_0eF_lxPzFBmotdloVfb4ikO4LBlFzP40dQfcGWT_sdz76auEVFnTUmqM170uZnI_2OWZfyRJwtUcmKceLPmN0oLXcm0xh_KEyRAmM2JbegS_DZAybUX56DaX5YeJzCRISXeQNbVDR8ZYvjxZPKCgbKtbqg-bSbA9S7CHpN6NpsbEfa153IKs2C-ismNLwm8VzE3baIKsVY"
+	});
+
+	expect(result2.ok()).toBeTruthy();
+
+	page.reload();
+
+	await expect(page.getByRole('heading', { name: 'example.com', level: 2 })).toHaveCount(1);
+	await expect(page.getByRole('heading', { name: 'example.org', level: 2 })).toHaveCount(1);
+
+	await expect(page.getByText('Admins', { exact: true })).toHaveCount(1);
+	await expect(page.getByText('Marketing', { exact: true })).toHaveCount(1);
+	await expect(page.getByText('Engineering', { exact: true })).toHaveCount(1);
+	await expect(page.getByText('Sales', { exact: true })).toHaveCount(1);
 });
